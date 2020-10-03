b511d838a5f4995e238044540a3be6e6060fe97c
Filip M <filip.moszczynski@onet.pl>
2020-10-03 18:40:38 +0200

    second commit before searching for bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Char } from './char.interface'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'technologie';
-}
+  public title = 'technologie';
+
+  public shouldComponentExist: boolean = true;
+
+  public chosenLetter: string = "";
+
+  public letterToSend = 'A';
+
+  public char: Char = {
+    letter: 'B',
+    id: 1
+  };
+
+  public onLetterClicked(letter: string): void {
+    console.log("Kliknięto:", letter);
+  }
+
+  public changeLetter(): void {
+    this.char = {
+      letter: "A",
+      id: 2
+    };
+  };
+
+  public toggleComponentVisibility(): void {
+      this.shouldComponentExist = !this.shouldComponentExist;
+  }
+
+  public letters: Char[] = [
+    {letter:"a", id:1},
+    {letter:"S", id:2},
+    {letter:"d", id:3},
+    {letter:"F", id:4}
+  ];
+
+
+  public choseLetter(letter: string): void{
+this.chosenLetter = this.chosenLetter;
+  }
+
+  // public classes = { 'clickedButton': chosenLetter === letter.letter};
+
+  public getClasses(letter: string): {'clicked-button': boolean} {
+    return {'clicked-button': this.chosenLetter === letter};
+  }
+
+};
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { AppCharItemComponent } from './app-char-item/app-char-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent,
+    MainComponent,
+    AppCharItemComponent
   ],
   imports: [
     BrowserModule
